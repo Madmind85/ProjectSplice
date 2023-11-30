dc092530a0591f8fe48fe626d0ec6e20b9ee6baa
Urb0rg <37514767+Urb0rg@users.noreply.github.com>
2023-11-30 20:01:29 +0100

    c interface

---
+++ Source/Mostriciattolo5/Interfaces/Int_Guardie.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mostriciattolo5/Interfaces/Int_Guardie.h"
+
+// Add default functionality here for any IInt_Guardie functions that are not pure virtual.
---
+++ Source/Mostriciattolo5/Interfaces/Int_Guardie.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Int_Guardie.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInt_Guardie : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MOSTRICIATTOLO5_API IInt_Guardie
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
