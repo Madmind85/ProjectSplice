03cb0c2ca7333bdafb547c72470751775393e9e0
Urb0rg <37514767+Urb0rg@users.noreply.github.com>
2024-03-17 15:10:54 +0100

    guardi c class

---
+++ Source/Mostriciattolo5/Public/Mostriciattolo5Guard.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Mostriciattolo5/Mostriciattolo5Character.h"
+#include "Mostriciattolo5Guard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOSTRICIATTOLO5_API AMostriciattolo5Guard : public AMostriciattolo5Character
+{
+	GENERATED_BODY()
+
+public:
+
+
+protected:
+
+
+private:
+	
+};
